9973d8cdd7e957fd8fcebd46aeb52d059097175a
tmwhitt <tmwhitt@umich.edu>
2018-11-29 12:02:45 -0500

    Uploading latest version of code that still uses the binary search method, which will require too many points. Switching to Simplex implementation

--- cavityParameterSelection.cpp
+++ cavityParameterSelection.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <fstream>
 #include <random>
+#include "omp.h"
 #include "timePropagation.h"
 
 using namespace std;
@@ -11,8 +12,9 @@ using namespace std;
 #define R_MIN 0.25
 #define R_MAX 0.75
 #define PI 3.141592653589793238463
-#define DIV_PER_DIM 2
+#define DIV_PER_DIM 5
 #define NUM_PULSES 9
+#define ITERATIONS 20
 
 void generateLandscape(	int numR, int numP, const int &numCavs, const int &numDivs, vector< vector<cavity> > &landscape, 
 						const vector< vector<double> > &reflVals, const vector< vector<double> > &phaseVals, 
@@ -56,13 +58,13 @@ void generateLandscapeSetup(const int &numCavs, const int &numDivs, vector< vect
 	return;
 }
 
-double MSE(const vector<double> &ideal_intensities, const vector<pulse> &calc){
+double MSE(const vector<double> &ideal_intensities, vector<pulse> &calc){
 	int numPoints = ideal_intensities.size();
 	double error = 0;
 	double temp;
 
 	for(int i=0;i<numPoints;++i){
-		temp = calc[i].real * calc[i].real + calc[i].imag * calc[i].imag;
+		temp = calc[i].getIntensity();
 		error += (ideal_intensities[i]-temp)*(ideal_intensities[i]-temp);
 	}
 
@@ -71,43 +73,136 @@ double MSE(const vector<double> &ideal_intensities, const vector<pulse> &calc){
 	return error;
 }
 
-int main (){
+int main (int argc, char *argv[]){
+	int i,j,k,best;
 	int numCavs = 4;
 	int numDivs = DIV_PER_DIM;
-	double phaseMin = 0;
-	double phaseMax = 2*PI;
-	double reflMin = R_MIN;
-	double reflMax = R_MAX;
+	int numIterations = ITERATIONS;
+
+	ofstream outfile;
+	outfile.open("testOut.txt");
+	if(!outfile){cout << "Error opening file" << endl;return -1;}
+
+	vector<double> phaseMin (numCavs,0);
+	vector<double> phaseMax (numCavs,2*PI);
+	vector<double> reflMin  (numCavs,R_MIN);
+	vector<double> reflMax  (numCavs,R_MAX);
+	double phaseStep, reflStep;
+	double minError = 1000000;
 
 	vector< vector<cavity> > landscape;
 	vector< vector<double> > reflVals(numCavs, vector<double>(numDivs));
 	vector< vector<double> > phaseVals(numCavs, vector<double>(numDivs));
 	vector<int> lengths{1,1,1,1};
 
+	default_random_engine generator;
+
 	// Create equal amplitude pulse train
-	vector<double> ideal_intensities(NUM_PULSES,1/NUM_PULSES); 
+	vector<double> ideal_intensities(NUM_PULSES,1.0/NUM_PULSES); 
+	vector<pulse> input(NUM_PULSES,0.0);
+	vector<pulse> output;
+	input[0].real = 1.0;
 
-	for(int i = 0; i < numCavs; ++i){
-		reflVals[i][0] = 0.5;
-		reflVals[i][1] = 0.6;
-		phaseVals[i][0] = -0.2;
-		phaseVals[i][1] = 0.9;
-	}
-
-	generateLandscapeSetup(numCavs,numDivs,landscape,reflVals,phaseVals,lengths);
-
-	int temp = 1 << numCavs;
-	
-	for(int i = 0; i < temp; ++i){
-		cout << "Values of cavity set " << i << endl;
-		for(int j = 0; j < numCavs; ++j){
-			cout << "Cavity " << j << ":" << endl;
-			cout << "Reflectivity: " << landscape[i][j].refl << endl;
-			cout << "Phase: " << landscape[i][j].phaseReal << ", " << landscape[i][j].phaseImag << endl;
-			cout << "Length: " << landscape[i][j].length << endl << endl;
+	for(k=0;k<numIterations;++k){
+		// Create points to test for each cavity
+		phaseStep = (phaseMax[0] - phaseMin[0]) / numDivs;
+		uniform_real_distribution<double> phaseDist(0.0,phaseStep);
+		reflStep = (reflMax[0] - reflMin[0]) / numDivs;
+		uniform_real_distribution<double> reflDist(0.0,reflStep);
+		for(i = 0; i < numCavs; ++i){
+			for(j=0;j<numDivs;++j){
+				reflVals[i][j] = reflMin[i] + j * reflStep + reflDist(generator);
+				phaseVals[i][j] = phaseMin[i] + j * phaseStep + phaseDist(generator);
+			}
 		}
-		cout << endl << endl;
+	
+		// Generate the landscape
+		generateLandscapeSetup(numCavs,numDivs,landscape,reflVals,phaseVals,lengths);
+		int numPoints = landscape.size();
+
+		// PARALLELIZE AND CALL DIFFERENT ONES HERE
+
+		// MPI Attempt
+		/*
+		int rank,numProc,rowStart,rowEnd;
+		MPI_Init(&argc,&argv);
+		MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+		MPI_Comm_size(MPI_COMM_WORLD,&numProc);
+
+		rowStart = rank * (numPoints / numProc);
+		if(numProc < numPoints % numProc){
+			rowStart += numProc;
+			rowEnd = rowStart + numPoints / numProc + 1;
+		}
+		else{
+			rowStart += numPoints % numProc;
+			rowEnd = rowStart + numPoints / numProc;
+		}
+
+		cout << "Rank=" << rank << ", start=" << rowStart << ", end=" << rowEnd << endl;
+		*/
+
+		#pragma omp parallel default(shared) firstprivate(input,output,ideal_intensities)
+		{
+			double minErrorLocal = 1000000;
+			int bestLocal;
+			double temp;
+			#pragma omp for private(i)
+			for(i=0;i<numPoints;++i){
+				cavityPropagation(landscape[i],input,output);
+				temp = MSE(ideal_intensities,output);
+				if(temp < minErrorLocal){
+					bestLocal = i;
+					minErrorLocal = temp;
+				}
+			}
+			#pragma omp critical
+			{
+				if(minErrorLocal < minError){
+					minError = minErrorLocal;
+					best = bestLocal;
+				}
+			}
+		}
+
+		// double temp;
+
+		// for(i=0;i<numPoints;++i){
+		// 	cavityPropagation(landscape[i],input,output);
+		// 	temp = MSE(ideal_intensities,output);
+		// 	if(temp < minError){
+		// 		best = i;
+		// 		minError = temp;
+		// 	}
+		// }
+
+		cout << "Used " << minError << " at " << best << endl;
+		cout << "Cavity 1: Reflectivity=" << landscape[best][0].getR() << endl;
+
+		// WILL NEED TO UPDATE MIN, MAX, AND STEP
+		double temp;
+		for(i=0;i<numCavs;++i){
+			temp = (reflMax[i] - reflMin[i])/4;
+			reflMax[i] = landscape[best][i].getR() + temp;
+			reflMin[i] = landscape[best][i].getR() - temp;
+			temp = (phaseMax[i] - phaseMin[i])/4;
+			phaseMax[i] = landscape[best][i].getR() + temp;
+			phaseMin[i] = landscape[best][i].getR() - temp;
+		}
+
+		cavityPropagation(landscape[best],input,output);
+
+		landscape.clear();
+		best = -1;
+		minError = 1000000;
+
+		cout << "Cavity 1 has maxRefl=" << reflMax[0] << " and minRefl=" << reflMin[0] << endl << endl;
+		outfile << "Input, Ideal, Output" << endl;
+		for(i=0;i<NUM_PULSES;++i){
+			outfile << input[i].getIntensity() << ", " << ideal_intensities[i] << ", " << output[i].getIntensity() << endl;
+		}
 	}
 	
+	outfile.close();
 	return 0;
 }
